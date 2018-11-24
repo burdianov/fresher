915adb70c1d1effcf35bd2e3d1cc6f62da4c703a
Lilian Burdianov <lilian.burdianov@gmail.com>
2018-11-24 22:23:03 +0400

    Register User

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\User;
 
 class RegisterController extends Controller
 {
-    public function register()
+    public function register(StoreUserRequest $request)
     {
+        $user = new User;
 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+
+        $user->save();
+
+        // response to be implemented later on
     }
 }
